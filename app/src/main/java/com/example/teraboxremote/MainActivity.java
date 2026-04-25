ef9e3e52accb02b9afba1d0753b8bbe47a14d639
wlasciciel11000-wq <257899229+wlasciciel11000-wq@users.noreply.github.com>
2026-04-25 05:05:35 -0400

    Fix: Simplify API call to resolve param error 36001

--- app/src/main/java/com/example/teraboxremote/MainActivity.java
+++ app/src/main/java/com/example/teraboxremote/MainActivity.java
@@ -154,15 +154,12 @@ public class MainActivity extends AppCompatActivity {
                 android.util.Log.d("TeraBox", "Adding task for URL: " + url);
                 // TeraBox/Baidu PCS API often requires additional parameters for remote upload to trigger correctly.
                 // We add 'channel=dubox', 'web=1', and 'clienttype=0' which are common in web requests.
-                String apiUrl = "https://www.terabox.com/rest/2.0/services/cloud_dl?method=add_task&app_id=" + APP_ID + "&ndus=" + ndus 
-                                + "&channel=dubox&web=1&clienttype=0";
-                
-                FormBody formBody = new FormBody.Builder()
-                        .add("save_path", "/From Link") // Changed from / to /From Link to avoid root permission issues
-                        .add("source_url", url)
-                        .add("type", "3")
-                        .add("timeout", "2147483647")
-                        .build();
+                String apiUrl = "https://www.terabox.com/rest/2.0/services/cloud_dl?method=add_task&app_id=250528" + "&ndus=" + ndus;
+	                
+	                FormBody formBody = new FormBody.Builder()
+	                        .add("save_path", "/")
+	                        .add("source_url", url)
+	                        .build();
 
                 Request request = new Request.Builder()
                         .url(apiUrl)
